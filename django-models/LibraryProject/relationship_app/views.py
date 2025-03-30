2573a3be177d08597ba78bdb55d1d8c76b04ebe2
DarkHero <essabery99@gmail.com>
2025-03-30 21:55:37 +0000

    commit

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,17 +1,15 @@
 from django.shortcuts import render
-from django.views.generic.detail import DetailView  # ✅ Explicitly importing DetailView
-from .models import Book, Library  # ✅ Explicitly importing Library
+from .models import Book
+from django.views.generic import DetailView
+from .models import Library
 
-# Function-Based View (FBV) for listing all books
 def list_books(request):
     books = Book.objects.all()
     return render(request, "relationship_app/list_books.html", {"books": books})
 
-# Class-Based View (CBV) for Library Details
 class LibraryDetailView(DetailView):
     model = Library
     template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
-
 
 # Create your views here.
