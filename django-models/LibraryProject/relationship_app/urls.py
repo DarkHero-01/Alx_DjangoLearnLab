5764b7e3d318ff8e5dcfb8342c79ed381f55bedf
DarkHero <essabery99@gmail.com>
2025-04-06 17:21:33 +0100

    commit

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -20,5 +20,5 @@ urlpatterns = [
 
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-    path('register/', views.RegisterView.as_view(), name='register'),
+    path('register/', views.registerView.as_view(), name='register'),
     ]
