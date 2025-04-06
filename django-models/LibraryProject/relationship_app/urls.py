f3cd5ec31e09f5b84b7728346448268759f09f51
DarkHero <essabery99@gmail.com>
2025-04-06 17:15:30 +0100

    commit

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from .views import list_books, LibraryDetailView
-from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 from .views import RegisterView, list_books, LibraryDetailView
 
