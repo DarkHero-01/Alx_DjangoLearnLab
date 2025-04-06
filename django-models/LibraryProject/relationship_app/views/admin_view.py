8b379d75f381258c58b57d1e0d9a14fdd2de61cc
DarkHero <essabery99@gmail.com>
2025-04-06 18:03:50 +0100

    COMMIT

--- django-models/LibraryProject/relationship_app/views/admin_view.py
+++ django-models/LibraryProject/relationship_app/views/admin_view.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponse
 from django.contrib.auth.decorators import user_passes_test
 
-def is_admin(user):
-    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+def is_admin(User):
+    return hasattr(User, 'UserProfile') and User.Userrofile.role == 'Admin'
 
 @user_passes_test(is_admin)
 def admin_view(request):
