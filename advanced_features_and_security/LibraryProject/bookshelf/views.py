9cac340c4e723709c7de54ce510a37de6af786d8
DarkHero <essabery99@gmail.com>
2025-04-09 22:14:00 +0100

    commit

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render, get_object_or_404
 from .models import Book
+from .forms import BookSearchForm
 
 @permission_required('bookshelf.can_view', raise_exception=True)
 def view_books(request):
@@ -27,5 +28,13 @@ def delete_book(request, book_id):
     book = get_object_or_404(Book, id=book_id)
     # Logic to handle deletion
     return render(request, 'bookshelf/delete_book.html', {'book': book})
+
+def search_books(request):
+    form = BookSearchForm(request.GET or None)
+    books = []
+    if form.is_valid():
+        query = form.cleaned_data.get('title')
+        books = Book.objects.filter(title__icontains=query)
+    return render(request, 'bookshelf/book_list.html', {'form': form, 'books': books})
 
 # Create your views here.
